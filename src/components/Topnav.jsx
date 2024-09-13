c5a82d4d964236d09976f827138335b21a160fd1
Amr Tarek <amr.tarek0012@gmail.com>
2024-09-13 15:57:47 +0300

    Update portfolio

--- src/components/Topnav.jsx
+++ src/components/Topnav.jsx
@@ -15,7 +15,7 @@ const Topnav = () => {
   return (
     <nav className="bg-white dark:bg-gray-900 fixed w-full z-20 top-0 start-0 border-b border-gray-200 dark:border-gray-600 h-16">
       <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto">
-        <a href="https://flowbite.com/" className="flex items-center space-x-3 rtl:space-x-reverse">
+        <a href="https://amrtarek0012.github.io/amr-portfolio/" className="flex items-center space-x-3 rtl:space-x-reverse">
           <span className="p-3 rounded">
             <Avatar img={Img} alt="avatar of Jese" />
           </span>
--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/amr-portfolio/", // YOUR REPO NAME HERE
+  base: "/amr-portfolio/",
 });
